a551b3266bbeded481280331ef01253fbebaa72c
jhonandres21 <jhonandresmedina@gmail.com>
2014-06-30 21:02:18 -0500

    Menu!

--- logico/Deportista.php
+++ logico/Deportista.php
@@ -2,118 +2,119 @@
 
 	class Deportista {
  	
-	private $indentificador, $nombre, $apellidos, $fechaNacimiento, $sexo, $identificacion, $tipoIdentificacion, $ciudad, $clubAfiliado, $nacionalidad, $estado, $edad, $categoria;
-    
-	public function getTipoIdentificacion() {
-        return $this->tipoIdentificacion;
-    }
+    	private $indentificador, $nombre, $apellidos, $fechaNacimiento, $sexo, $identificacion;
+        private $tipoIdentificacion, $ciudad, $clubAfiliado, $nacionalidad, $estado, $edad, $categoria;
+        
+    	public function getTipoIdentificacion() {
+            return $this->tipoIdentificacion;
+        }
 
-    public function setEdad($edad) {
-        $this->edad = $edad;
-    }
+        public function setEdad($edad) {
+            $this->edad = $edad;
+        }
 
-    public function setCategoria($categoria) {
-        $this->categoria = $categoria;
-    }
+        public function setCategoria($categoria) {
+            $this->categoria = $categoria;
+        }
 
-    public function getEdad() {
-        return $this->edad;
-    }
+        public function getEdad() {
+            return $this->edad;
+        }
 
-    public function setIndentificador($indentificador) {
-        $this->indentificador = $indentificador;
-    }
+        public function setIndentificador($indentificador) {
+            $this->indentificador = $indentificador;
+        }
 
-    public function setNombre($nombre) {
-        $this->nombre = $nombre;
-    }
+        public function setNombre($nombre) {
+            $this->nombre = $nombre;
+        }
 
-    public function setApellidos($apellidos) {
-        $this->apellidos = $apellidos;
-    }
+        public function setApellidos($apellidos) {
+            $this->apellidos = $apellidos;
+        }
 
-    public function setFechaNacimiento($fechaNacimiento) {
-        $this->fechaNacimiento = $fechaNacimiento;
-    }
+        public function setFechaNacimiento($fechaNacimiento) {
+            $this->fechaNacimiento = $fechaNacimiento;
+        }
 
-    public function setSexo($sexo) {
-        $this->sexo = $sexo;
-    }
-    
-    public function setTipoIdentificacion($tipoIdentificacion){
-        $this->tipoIdentificacion = $tipoIdentificacion;
-    }
-    
-    public function setIdentificacion($identificacion) {
-        $this->identificacion = $identificacion;
-    }
+        public function setSexo($sexo) {
+            $this->sexo = $sexo;
+        }
+        
+        public function setTipoIdentificacion($tipoIdentificacion){
+            $this->tipoIdentificacion = $tipoIdentificacion;
+        }
+        
+        public function setIdentificacion($identificacion) {
+            $this->identificacion = $identificacion;
+        }
 
-    public function setTiempoIdentificacion($tipoIdentificacion) {
-        $this->tipoIdentificacion = $tipoIdentificacion;
-    }
+        public function setTiempoIdentificacion($tipoIdentificacion) {
+            $this->tipoIdentificacion = $tipoIdentificacion;
+        }
 
-    public function setCiudad($ciudad) {
-        $this->ciudad = $ciudad;
-    }
+        public function setCiudad($ciudad) {
+            $this->ciudad = $ciudad;
+        }
 
-    public function setClubAfiliado($clubAfiliado) {
-        $this->clubAfiliado = $clubAfiliado;
-    }
+        public function setClubAfiliado($clubAfiliado) {
+            $this->clubAfiliado = $clubAfiliado;
+        }
 
-    public function setNacionalidad($nacionalidad) {
-        $this->nacionalidad = $nacionalidad;
-    }
+        public function setNacionalidad($nacionalidad) {
+            $this->nacionalidad = $nacionalidad;
+        }
 
-    public function setEstado($estado) {
-        $this->estado = $estado;
-    }
+        public function setEstado($estado) {
+            $this->estado = $estado;
+        }
 
-    public function getIndentificador() {
-        return $this->indentificador;
-    }
+        public function getIndentificador() {
+            return $this->indentificador;
+        }
 
-    public function getNombre() {
-        return $this->nombre;
-    }
+        public function getNombre() {
+            return $this->nombre;
+        }
 
-    public function getApellidos() {
-        return $this->apellidos;
-    }
+        public function getApellidos() {
+            return $this->apellidos;
+        }
 
-    public function getFechaNacimiento() {
-        return $this->fechaNacimiento;
-    }
+        public function getFechaNacimiento() {
+            return $this->fechaNacimiento;
+        }
 
-    public function getSexo() {
-        return $this->sexo;
-    }
+        public function getSexo() {
+            return $this->sexo;
+        }
 
-    public function getIdentificacion() {
-        return $this->identificacion;
-    }
+        public function getIdentificacion() {
+            return $this->identificacion;
+        }
 
-    public function getTiempoIdentificacion() {
-        return $this->tipoIdentificacion;
-    }
+        public function getTiempoIdentificacion() {
+            return $this->tipoIdentificacion;
+        }
 
-    public function getCiudad() {
-        return $this->ciudad;
-    }
+        public function getCiudad() {
+            return $this->ciudad;
+        }
 
-    public function getClubAfiliado() {
-        return $this->clubAfiliado;
-    }
+        public function getClubAfiliado() {
+            return $this->clubAfiliado;
+        }
 
-    public function getNacionalidad() {
-        return $this->nacionalidad;
-    }
+        public function getNacionalidad() {
+            return $this->nacionalidad;
+        }
 
-    public function getEstado() {
-        return $this->estado;
-    }
+        public function getEstado() {
+            return $this->estado;
+        }
 
-    public function getCategoria() {
-        return $this->categoria;
-    }
-}
+        public function getCategoria() {
+            return $this->categoria;
+        }
+    }//Fin clase
 ?>
